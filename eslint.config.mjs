7b22c81215e02f79c8d3589579073083d25df9af
Muhammed Abdrahman <muhammed03amin@gmail.com>
2024-10-30 00:35:09 +0500

    fix: fix eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,36 @@
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginReact.configs.flat.recommended,
   {
-    // extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    rules: {
-      "react/react-in-jsx-scope": "off"
-    }
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
   },
   {
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:prettier/recommended",
-    ],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: {
+      react: pluginReact,
+      prettier: pluginPrettier,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginPrettier.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error',
+    },
   },
 ];
